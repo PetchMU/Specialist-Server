5a7565819fe9cae5e9f8d734fc90935e77973e2f
User-PC\User <petch.kr@gmail.com>
2017-09-07 15:40:23 +0700

    no message

--- api.php
+++ api.php
@@ -1,10 +1,12 @@
 <?php
 
+
 include 'helpers/Router.php';
 
 $router = new Router();
 include 'router.php';
 
+$found = $router->setConteollerPath(__DIR__ . '/controllers');
 $found = $router->run();
 
 if(!$found){
